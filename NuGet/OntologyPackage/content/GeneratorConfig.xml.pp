af0c7fba2b8e47a1e8a0b9920848ef24602d0432
Moritz Eberl <2842200+meberl@users.noreply.github.com>
2015-06-16 15:42:12 +0200

    Prepared solution for nuget packaging.

---
+++ NuGet/OntologyPackage/content/GeneratorConfig.xml.pp
@@ -0,0 +1,21 @@
+﻿<GeneratorConfiguration>
+
+  <Namespace>$DefaultNamespace$.Ontologies</Namespace>
+  <!--http://www.w3.org/2002/07/owl#-->
+  <Ontology Uri="http://www.w3.org/2002/07/owl#" Prefix="owl">
+    <FileSource Location="Ontologies\owl.n3" />
+  </Ontology>
+
+  <!--http://www.w3.org/1999/02/22-rdf-syntax-ns#-->
+  <Ontology Uri="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    Prefix="rdf">
+    <FileSource Location="Ontologies\rdf.rdf" />
+  </Ontology>
+
+  <!--http://www.w3.org/2000/01/rdf-schema#-->
+  <Ontology Uri="http://www.w3.org/2000/01/rdf-schema#" Prefix="rdfs">
+    <FileSource Location="Ontologies\rdfs.n3" />
+  </Ontology>
+
+</GeneratorConfiguration>
+
---
+++ NuGet/OntologyPackage/content/Ontologies.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Semiodesk.Trinity;
+
+namespace Semiodesk.Trinity.Ontologies
+{
+
+}
