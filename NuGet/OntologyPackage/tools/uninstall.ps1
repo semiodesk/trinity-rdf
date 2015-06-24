e66525db44a52f493d0ea53155f85698c141bc8b
Moritz Eberl <2842200+meberl@users.noreply.github.com>
2015-06-24 14:45:51 +0200

    Fixed nuget for ontologies package.

--- NuGet/OntologyPackage/tools/uninstall.ps1
+++ NuGet/OntologyPackage/tools/uninstall.ps1
@@ -2,18 +2,19 @@ param($installPath, $toolsPath, $package, $project)
 
 $cmds = $project.Properties.Item("PreBuildEvent").Value
 
+$cdCmd = 'CD "$(SolutionDir)packages\Semiodesk.Trinity.Ontologies*\tools"'
 $buildCmd = 'OntologyGenerator.exe'
 $deployCmd = 'OntologyDeployment.exe'
 
 
-if ($cmds.Contains($buildCmd) -or $cmds.Contains($deployCmd) )
+if ($cmds.Contains($buildCmd) -or $cmds.Contains($deployCmd) -or $cmds.Contains($cdCmd) )
 {
     $lines = [regex]::Split($cmds,"\r\n")
     $newCmds = ""
    
    foreach($line in $lines)
    {
-        if($line.Length -gt 0 -and !$line.Contains($buildCmd) -and !$line.Contains($deployCmd))
+        if($line.Length -gt 0 -and !$line.Contains($buildCmd) -and !$line.Contains($deployCmd) -and !$line.Contains($cdCmd))
         {
             $newCmds += $line + "`r`n"  # Creating a cleaned list of commands
         }
