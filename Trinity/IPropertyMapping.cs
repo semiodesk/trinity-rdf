01ccb646b2e6d3ee3498e07da7d94d2240847097
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2015-06-26 11:09:37 +0200

    Resource does not raise the INotifyPropertyChanged for mapped properties by default. This is now handled by the CilGenerator for the properties individually. Added functions for registering manually implemented INotifyProperty changed properties for raising the event on rollback.

---
+++ Trinity/IPropertyMapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Semiodesk.Trinity
+{
+    internal interface IPropertyMapping
+    {
+        Type DataType { get; }
+
+        Type GenericType { get; }
+
+        bool IsList { get; }
+
+        Property Property { get; }
+
+        string PropertyName { get; }
+
+        bool IsUnsetValue { get; }
+
+        bool IsTypeCompatible(Type type);
+
+        object GetValueObject();
+
+        void SetOrAddMappedValue(object value);
+
+        void RemoveOrResetValue(object value);
+
+        void CloneFrom(IPropertyMapping other);
+    }
+}
