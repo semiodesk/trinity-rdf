b7f202199f5a8d8a270821ab4b5cb6cea397d25b
Moritz Eberl <2842200+meberl@users.noreply.github.com>
2015-07-01 16:41:21 +0200

    Switched OntologyGenerator to MSBuild Task.

---
+++ Trinity.OntologyDeployment/Task/DeployOntology.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace Semiodesk.Trinity.OntologyDeployment.Task
+{
+    class DeployOntology : ITask
+    {
+        #region ITask Members
+
+        public IBuildEngine BuildEngine
+        {
+            get;
+            set;
+        }
+
+        public bool Execute()
+        {
+            
+            //BuildEngine.ProjectFileOfTaskNode
+             //FileInfo assembly = System.Reflection.Assembly.GetExecutingAssembly().Location
+
+            return true;
+        }
+
+        public ITaskHost HostObject
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
