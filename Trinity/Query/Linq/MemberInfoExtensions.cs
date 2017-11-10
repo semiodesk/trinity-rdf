33cfecee89ffd9b719a42e26b9f02d216ab5ecbe
Sebastian Faubel <sebastian@semiodesk.com>
2017-11-10 13:03:09 +0100

    Implementing aggreate support.

--- Trinity/Query/Linq/MemberInfoExtensions.cs
+++ Trinity/Query/Linq/MemberInfoExtensions.cs
@@ -12,5 +12,10 @@ namespace Semiodesk.Trinity.Query
 
             return member.GetCustomAttributes(attributeType, true).FirstOrDefault() as RdfPropertyAttribute;
         }
+
+        public static TAttribute TryGetCustomAttribute<TAttribute>(this MemberInfo member) where TAttribute : Attribute
+        {
+            return member.GetCustomAttributes(typeof(TAttribute), true).FirstOrDefault() as TAttribute;
+        }
     }
 }
---
+++ Trinity/Query/Linq/VariableBuilder.cs
@@ -0,0 +1,26 @@
+﻿using VDS.RDF.Query;
+
+namespace Semiodesk.Trinity.Query
+{
+    internal class VariableBuilder
+    {
+        #region Members
+
+        private int _objectCount;
+
+        #endregion
+
+        #region Methods
+
+        public SparqlVariable GenerateObjectVariable(string prefix = "o")
+        {
+            string v = prefix + _objectCount;
+
+            _objectCount += 1;
+
+            return new SparqlVariable(v);
+        }
+
+        #endregion
+    }
+}
