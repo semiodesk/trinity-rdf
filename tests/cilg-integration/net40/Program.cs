c031bacd59c6f685e44d05f9002cdd27eae9ee60
Moritz Eberl <2842200+meberl@users.noreply.github.com>
2018-03-08 18:35:01 +0100

    Updated Net35 with to new cecil version. Added integration tests for cilg. Updated cake build to run integration tests

---
+++ tests/cilg-integration/net40/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace NetCore_Test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var agent = new Agent(new Uri("ex:Test"));
+            agent.Name = "August";
+            agent.Commit();
+            if (agent.ListValues(foaf.name).Count() > 0)
+            {
+                Console.WriteLine("true");
+                return 0;
+            }
+            Console.WriteLine("false");
+            return -1;
+        }
+    }
+}
