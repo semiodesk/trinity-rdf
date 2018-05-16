050790bec9e7503b4b561b5366037f8b87321a53
Moritz Eberl <moritz@semiodesk.com>
2018-05-16 08:08:59 +0200

    Added blueprint project for nuget integration test.

---
+++ tests/nuget-integration/blueprint/netcore/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Semiodesk.Trinity;
+
+namespace NugetIntegrationTest
+{
+
+    [RdfClass("http://xmlns.com/foaf/0.1/Agent")]
+    public class Agent : Resource
+    {
+        public Agent(Uri uri)
+            : base(uri)
+        {
+        }
+
+        [RdfProperty("http://xmlns.com/foaf/0.1/name")]
+        public string Name { get; set; }
+
+        [RdfProperty("http://xmlns.com/foaf/0.1/mbox")]
+        public List<Resource> EMailAccounts { get; set; }
+    }
+
+}
