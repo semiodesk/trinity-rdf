4c569a254a60d249ca9327582feaae8189e3307a
Moritz Eberl <2842200+meberl@users.noreply.github.com>
2018-05-18 22:16:41 +0200

    Updated nuget tests, still not working :(

--- tests/nuget-integration/blueprint/common/Program.cs
+++ tests/nuget-integration/blueprint/common/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Semiodesk.Trinity;
 
 namespace NugetIntegrationTest
 {
@@ -9,11 +10,18 @@ namespace NugetIntegrationTest
     {
         static int Main(string[] args)
         {
-            var agent = new Agent(new Uri("ex:Test"));
+            var Store = StoreFactory.CreateStore("provider=dotnetrdf");
+            Uri testModel = new Uri("ex:Test");
+            var Model = Store.CreateModel(testModel);
+
+            var agentUri = new Uri("ex:Test");
+            var agent = Model.CreateResource<Agent>(agentUri);
             agent.Name = "August";
             agent.Commit();
+
+            var result = Model.GetResource<Agent>(agentUri);
             // This line serves two purposes, it tests the ontology generation as well as cilg
-            if (agent.ListValues(foaf.name).Count() > 0)
+            if (result.ListValues(foaf.name).Count() > 0)
             {
                 Console.WriteLine("true");
                 return 0;
