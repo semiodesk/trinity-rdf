4effdbb33da71217338ed7710528d18cc32aebe5
Moritz Eberl <moritz@semiodesk.com>
2022-09-08 08:51:30 +0200

    Fixed sign script

--- sign.ps1
+++ sign.ps1
@@ -1,3 +1,2 @@
-nuget sign Build\Release\Semiodesk.Trinity.*.nupkg -Timestamper http://sha256timestamp.ws.symantec.com/sha256/timestamp  -CertificateFingerprint 0b5f624d32cb7e5549405b32a8a0a9aebfd1859a
+dotnet nuget sign Build\Release\Semiodesk.Trinity.*.nupkg --timestamper http://sha256timestamp.ws.symantec.com/sha256/timestamp  --certificate-fingerprint 0b5f624d32cb7e5549405b32a8a0a9aebfd1859a
 
-nuget sign Build\Release\stores\virtuoso\Semiodesk.Trinity.Virtuoso.*.nupkg -Timestamper http://sha256timestamp.ws.symantec.com/sha256/timestamp  -CertificateFingerprint 0b5f624d32cb7e5549405b32a8a0a9aebfd1859a
